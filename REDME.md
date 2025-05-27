14f4d66a5289277cf27be0df7e15fa45c137098b
s31255 <s31255@pjwstk.edu.pl>
2025-05-27 11:31:47 +0200

    add REDME and /health

---
+++ REDME.md
@@ -0,0 +1,4 @@
+Ten projekt jest szablonem jak powinno się robić aplikacje w kotekście developmentu
+zawierający pokazane i zrobione jak dodać i skonfigurować workflowsy i github action
+do automatycznych testów dockerfile requirements oraz monitorowanie pracy poprzez
+/health
--- hello.py
+++ hello.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def hello():
     return jsonify(status="ok", time=datetime.now().isoformat())
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
+print(">>> App started:", datetime.now())
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
